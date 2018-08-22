f45b5122069448847b4625eafdde9ecd423f8a3c
Ladislav Prskavec <ladislav.prskavec@oracle.com>
2018-08-22 15:38:53 +0200

    chore: update dockerfile and setup script for ironmq

--- scripts/setup-ironmq.sh
+++ scripts/setup-ironmq.sh
@@ -1,7 +1,11 @@
 export USER_EMAIL="testing-ironmq-user@apiary-internal.com"
 export USER_PASSWORD="testpassword"
 export PROJECT_NAME="ivy-test-suite"
-export USER_TOKEN=`docker run --net=host iron/authcli iron -t adminToken create user $USER_EMAIL $USER_PASSWORD | grep -o '[^=]*$'`
-export PROJECT_ID=`docker run --net=host iron/authcli iron -t $USER_TOKEN create project $PROJECT_NAME | cut -d' ' -f6 | cut -d'=' -f2`
+# get errors if project/user exists
+docker run --net=host iron/authcli iron -t adminToken create user $USER_EMAIL $USER_PASSWORD
+docker run --net=host iron/authcli iron -t $USER_TOKEN create project $PROJECT_NAME
+# always works if project/user already exists
+export USER_TOKEN=`docker run --net=host iron/authcli iron -t adminToken get user $USER_EMAIL |  cut -d' ' -f7 | cut -d'=' -f2 | tr -d []`
+export PROJECT_ID=`docker run --net=host iron/authcli iron -t $USER_TOKEN get projects | cut -d' ' -f5 | cut -d'=' -f2`
 
 echo "{\"token\": \"$USER_TOKEN\", \"project_id\": \"$PROJECT_ID\"}" > iron.json
